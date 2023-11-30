c47daa573abaabe6037f3a0708c6e5d07c6c1e44
dmerk2 <dan.merkin@gmail.com>
2023-11-29 23:14:15 -0500

    Creating server with socket connection

---
+++ server/server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const path = require("path");
+// const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { authMiddleware } = require("./utils/auth");
+const db = require("./config/connection");
+// const { typeDefs, resolvers } = require("./schemas");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+// Comment out ApolloServer instantiation for testing
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// });
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Comment out ApolloServer middleware for testing
+// app.use("/graphql", expressMiddleware(server));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+}
+
+// WebSocket logic
+io.on("connection", (socket) => {
+  console.log("User connected socket ID:", socket.id);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User ID: ${socket.id} joined room ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+    console.log("Receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", socket.id);
+  });
+});
+
+db.once("open", () => {
+  httpServer.listen(PORT, () => {
+    console.log(`API server running on http://localhost:${PORT}!`);
+    // Comment out GraphQL info for testing
+    // console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
+});
