93396257fbe3042c30cb2ca1df9a8f2c38c33674
dmerk2 <dan.merkin@gmail.com>
2023-11-30 17:06:07 -0500

    Creating Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,39 @@
+import { Link } from "react-router-dom";
+import styles from "../styles/Header.module.css";
+import Logo from "../assets/images/BranchOut_With_Words.png";
+
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className={styles.header}>
+      <Link to="/" className={styles.headerLogo}>
+        <img src={Logo} alt="Branch Out Logo" />
+        Branch Out
+      </Link>
+      <div className={styles.headerSearch}>
+        <input type="search" placeholder="Search..." />
+      </div>
+      <div className={styles.headerNav}>
+        <ul className={styles.navbarNav}>
+          <li>
+            <Link to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile">
+              Profile
+            </Link>
+          </li>
+          <li>
+            <Link to="/chatroom">
+              Live Chat
+            </Link>
+          </li>
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 }
