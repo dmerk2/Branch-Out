e16c00b0c29af9e47d2081ebe5e94fa20030b219
Robert Kaiser <paintballmanrob@gmail.com>
2023-11-30 19:52:05 -0500

    Add mutation types to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,6 +33,20 @@ const typeDefs = `
     posts: [Post]
     comments: [Comment]
   }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): User
+    updateUser(_id: ID!, username: String, email: String, password: String): User
+    deleteUser(_id: ID!): User
+
+    addPost(user: ID!, content: String!): Post
+    updatePost(_id: ID!, content: String): Post
+    deletePost(_id: ID!): Post
+
+    addComment(post: ID!, user: ID!, content: String!): Comment
+    updateComment(_id: ID!, content: String): Comment
+    deleteComment(_id: ID!): Comment
+  }
 `;
 
 module.exports = typeDefs;
