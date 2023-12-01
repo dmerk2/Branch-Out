15a494e3f58a980302df2e0af961ed8fd3ba54a8
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-12-01 18:42:47 -0500

    Adding Font Awesome icons, and set up User Diamond

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+import "../index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 
 export default function Profile() {
   return (
-    <div>
-      Profile
-    </div>
-  )
+    <body>
+      <div class="row">
+        <div className="column">
+          <div className="user-info">
+            <div className="flex-container">
+              <div className="diamond-shape">
+                <FontAwesomeIcon icon={faUserSecret} className="user-icon" />
+              </div>
+              <p className="username">SampleUser281</p>
+            </div>
+          </div>
+          <div className="user-actions">
+            <button className="action-button discord-button">
+              <FontAwesomeIcon icon={faUserSecret} className="action-icon" />
+            </button>
+            <button className="action-button github-button">
+              <FontAwesomeIcon icon={faUserSecret} className="action-icon" />
+            </button>
+          </div>
+          <div className="friends-info">
+            <p className="friends-count">Friends: 21</p>
+            <button className="view-all-button">View All</button>
+          </div>
+          <div className="rectangle rectangle1"></div>
+          <div className="rectangle rectangle2"></div>
+        </div>
+        <div class="column">Column 2</div>
+        <div class="column">Column 3</div>
+      </div>
+    </body>
+  );
 }
