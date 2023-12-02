13937a39cf7c5788568e81440f040e08433545aa
Robert Kaiser <paintballmanrob@gmail.com>
2023-12-01 22:21:39 -0500

    Update import paths in App.jsx and main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import "./index.css";
+import "./styles/index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import store from "./utils/store";
+// import store from "./app/store.js";
 import Home from "./layouts/Home.jsx";
 import Profile from "./layouts/Profile.jsx";
 import ChatRoom from "./layouts/ChatRoom.jsx";
@@ -57,8 +57,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    {/* <Provider store={store}> */}
+    <RouterProvider router={router} />
+    {/* </Provider> */}
   </React.StrictMode>
 );
