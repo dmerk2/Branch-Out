477b7081d5bacf8503b7f32743b32a2fec31962e
dmerk2 <dan.merkin@gmail.com>
2023-12-01 22:19:50 -0500

    Fixing bugs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import store from "./utils/store";
+// import store from "./utils/store";
 import Home from "./pages/Home.jsx";
 import Profile from "./pages/Profile.jsx";
 import ChatRoom from "./pages/ChatRoom.jsx";
@@ -57,8 +57,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <RouterProvider router={router} />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
