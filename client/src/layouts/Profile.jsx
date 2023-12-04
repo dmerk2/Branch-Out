06d57fcccaa1b6eea737d8e67c4353fb6bf2e651
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-12-03 21:23:01 -0500

    Added Profile Buttons and fixed css for About Me

--- client/src/layouts/Profile.jsx
+++ client/src/layouts/Profile.jsx
@@ -7,6 +7,7 @@ import ProfileUserDisplay from "../common/components/ProfileUserDisplay.jsx";
 import ViewFriendsList from "../common/components/ViewFriendsList.jsx";
 import AddFriendSquare from "../common/components/AddFriendSquare.jsx";
 import AboutMe from "../common/components/AboutMe.jsx";
+import DiamondButton from "../common/components/DiamondButton.jsx";
 
 export default function Profile() {
   return (
@@ -34,6 +35,11 @@ export default function Profile() {
         </div>
         <div className= { styles.profilePageColumn}>
           <AboutMe />
+          <div className={styles.profileButtonRow}>
+            <div className={styles.profileButton}><DiamondButton /></div>
+            <div className={styles.profileButton}><DiamondButton /></div>
+            <div className={styles.profileButton}><DiamondButton /></div>
+          </div>
         </div>
         <div className= { styles.profilePageColumn}>Column 3</div>
       </div>
