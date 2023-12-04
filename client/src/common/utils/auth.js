d0031d6634dc0837c6417ea3f1fa7ee14ef3b574
dmerk2 <dan.merkin@gmail.com>
2023-12-04 18:42:33 -0500

    Creating login and ADD_USER mutation for client

--- client/src/common/utils/auth.js
+++ client/src/common/utils/auth.js
@@ -38,12 +38,12 @@ class AuthService {
     window.location.assign('/');
   }
 
-  // logout() {
-  //   // Clear user token and profile data from localStorage
-  //   localStorage.removeItem('id_token');
-  //   // this will reload the page and reset the state of the application
-  //   window.location.assign('/');
-  // }
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // this will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
 }
 
 export default new AuthService();
