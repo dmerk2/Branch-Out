802a03b8afd7c8cadcf3c3e5f6d55046017fb190
Robert Kaiser <paintballmanrob@gmail.com>
2023-12-05 19:51:52 -0500

    Fix import and add username and email existence check in SignUpForm.jsx

--- client/src/common/components/SignUpForm.jsx
+++ client/src/common/components/SignUpForm.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
-import { useMutation } from "@apollo/client";
+import { useLazyQuery, useMutation } from "@apollo/client";
 import { ADD_USER, GET_PRESIGNED_URL } from "../utils/mutations";
+import { CHECK_USERNAME_EXISTS } from "../utils/queries";
 import Auth from "../utils/auth";
 import { BarLoader as Spinner } from "react-spinners";
 import styles from "../../styles/LoginForm.module.css";
 import Logo from "../../assets/images/BranchOut_With_Words.png";
-
 
 function SignUpForm() {
   const [userFormData, setUserFormData] = useState({
@@ -19,9 +19,13 @@ function SignUpForm() {
   const [isUploading, setIsUploading] = useState(false);
   const [color, setColor] = useState("#FF0000"); // Can be a CSS hex-color string or an array of colors in hex format
   const [uploadProgress, setUploadProgress] = useState(0);
+  const [submitAttempted, setSubmitAttempted] = useState(false);
   const [addUser, { error: addUserError }] = useMutation(ADD_USER);
   const [getPresignedUrl, { error: presignedUrlError }] =
     useMutation(GET_PRESIGNED_URL);
+  const [checkUsernameEmailExists, { data, loading, error }] = useLazyQuery(
+    CHECK_USERNAME_EMAIL_EXISTS
+  );
 
   useEffect(() => {
     presignedUrlError ? setShowAlert(true) : setShowAlert(false);
@@ -30,6 +34,45 @@ function SignUpForm() {
   useEffect(() => {
     addUserError ? setShowAlert(true) : setShowAlert(false);
   }, [addUserError]);
+
+  useEffect(() => {
+    if (submitAttempted) {
+      if (loading) return; // Waiting for the query to complete
+
+      if (error) {
+        console.error("Error checking username and email:", error);
+        setShowAlert(true);
+        setIsUploading(false);
+        setSubmitAttempted(false);
+        return;
+      }
+
+      if (data && data.users.length > 0) {
+        const duplicateField = data.users.some(
+          (user) => user.username === userFormData.username
+        )
+          ? "username"
+          : "email";
+        const message =
+          duplicateField === "username"
+            ? "This username is already taken. Please choose a different username."
+            : "This email is already registered. Please use a different email.";
+        alert(message);
+        setIsUploading(false);
+      } else {
+        // Proceed with file upload and form submission
+        // [place your file upload and form submission logic here]
+      }
+      setSubmitAttempted(false);
+    }
+  }, [
+    submitAttempted,
+    data,
+    loading,
+    error,
+    userFormData.username,
+    userFormData.email,
+  ]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -44,7 +87,16 @@ function SignUpForm() {
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-    setIsUploading(true); // Start upload
+    setIsUploading(true); // Indicate the start of the submission process
+
+    // Trigger the lazy query to check if the username or email exists
+    checkUsernameEmailExists({
+      variables: {
+        username: userFormData.username,
+        email: userFormData.email,
+      },
+    });
+    setSubmitAttempted(true); // Indicate that a submission attempt has been made
 
     try {
       const file = userFormData.profileImage;
@@ -58,42 +110,57 @@ function SignUpForm() {
       const formData = new FormData();
       formData.append("file", file);
 
-      const uploadResponse = await fetch(presignedUrl, {
-        method: "PUT",
-        body: formData,
-        // onUploadProgress: implement if using XMLHttpRequest or Axios
-      });
+      // Create a new XMLHttpRequest
+      var xhr = new XMLHttpRequest();
 
-      if (uploadResponse.ok) {
-        const imageUrl = `https://branch-out-images.s3.amazonaws.com/${key}`;
-        const updatedFormData = { ...userFormData, profileImage: imageUrl };
+      // Setup our listener to process completed requests
+      xhr.onload = async function () {
+        if (xhr.status == 200) {
+          const imageUrl = `https://branch-out-images.s3.amazonaws.com/${key}`;
+          const updatedFormData = { ...userFormData, profileImage: imageUrl };
 
-        // Submit the user data
-        const { data } = await addUser({ variables: { ...updatedFormData } });
-        Auth.login(data.addUser.token);
-      } else {
-        throw new Error("Failed to upload image to S3");
-      }
+          // Submit the user data
+          const { data } = await addUser({ variables: { ...updatedFormData } });
+          Auth.login(data.addUser.token);
+        } else {
+          throw new Error("Failed to upload image to S3");
+        }
+      };
+
+      // Setup our listener to process upload progress
+      xhr.upload.onprogress = function (e) {
+        if (e.lengthComputable) {
+          setUploadProgress((e.loaded / e.total) * 100);
+        }
+      };
+
+      // Open the connection
+      xhr.open("PUT", presignedUrl, true);
+
+      // Send the file
+      xhr.send(formData);
     } catch (error) {
-      console.error("Error in form submission:", error.stack);
-      console.log("Current userFormData state:", userFormData);
-      // Specifically log the profileImage data
-      console.log("Profile Image Data:", userFormData.profileImage);
+      console.error("Error signing up user:", error);
+      setShowAlert(true);
     } finally {
       setIsUploading(false); // Reset uploading state
     }
   };
-
   return (
     <>
-      <form noValidate onSubmit={handleFormSubmit}  className={styles.formSquare}>
-
-      <div className={styles.formLogo}>
-            <img src={Logo} alt="Branch Out Logo" />
+      <form
+        noValidate
+        onSubmit={handleFormSubmit}
+        className={styles.formSquare}
+      >
+        <div className={styles.formLogo}>
+          <img src={Logo} alt="Branch Out Logo" />
         </div>
 
         <div>
-          <label htmlFor="email" className={styles.loginRequirement}>Email</label>
+          <label htmlFor="email" className={styles.loginRequirement}>
+            Email
+          </label>
           <input
             className={styles.loginInput}
             type="text"
@@ -107,7 +174,9 @@ function SignUpForm() {
         </div>
 
         <div>
-          <label htmlFor="username" className={styles.loginRequirement}>Username</label>
+          <label htmlFor="username" className={styles.loginRequirement}>
+            Username
+          </label>
           <input
             className={styles.loginInput}
             type="text"
@@ -121,7 +190,9 @@ function SignUpForm() {
         </div>
 
         <div>
-          <label htmlFor="password" className={styles.loginRequirement}>Password</label>
+          <label htmlFor="password" className={styles.loginRequirement}>
+            Password
+          </label>
           <input
             className={styles.loginInput}
             type="password"
@@ -135,7 +206,9 @@ function SignUpForm() {
         </div>
 
         <div>
-          <label htmlFor="bio" className={styles.loginRequirement}>Bio</label>
+          <label htmlFor="bio" className={styles.loginRequirement}>
+            Bio
+          </label>
           <textarea
             className={styles.loginInput}
             name="bio"
@@ -149,8 +222,9 @@ function SignUpForm() {
         </div>
 
         <div>
-
-          <label htmlFor="profileImage"  className={styles.loginRequirement}>Profile Picture</label>
+          <label htmlFor="profileImage" className={styles.loginRequirement}>
+            Profile Picture
+          </label>
           <input
             className={styles.loginInput}
             type="file"
@@ -179,10 +253,14 @@ function SignUpForm() {
           )}
         </div>
 
-        <button type="submit" className={styles.signUpPageButton} disabled={isUploading}>
+        <button
+          type="submit"
+          className={styles.signUpPageButton}
+          disabled={isUploading}
+        >
           Submit
         </button>
-        
+
         {showAlert && (
           <div className={styles.somethingWrong}>
             Something went wrong with your sign up!
