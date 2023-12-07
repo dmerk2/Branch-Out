ae3a6a1cb89eea24ef0b07abf7a2ab97a5f3c0a6
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-12-07 18:10:49 -0500

    Added Styling to About Branch Out

--- client/src/common/components/AboutBranchOut.jsx
+++ client/src/common/components/AboutBranchOut.jsx
@@ -3,8 +3,8 @@ import styles from '../../styles/AboutBranchOut.module.css';
 export default function AboutBranchOut() {
     return (
         <div className={styles.bigGreenSquare}>
-            <p className={styles.aboutMe}>About BranchOut</p>
-            <p className={styles.userDescription}> BranchOut is a social media platform that allows users to connect with other users and share their thoughts and ideas. </p>
+            <p className={styles.aboutBranchOut}>About BranchOut</p>
+            <p className={styles.branchOutDescription}> BranchOut is a social media platform that allows users to connect with other users and share their thoughts and ideas. </p>
         </div>
     );
 }
