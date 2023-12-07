187babebd3ff8110a4de3f1c9587dd22ca09efdf
dmerk2 <dan.merkin@gmail.com>
2023-12-06 20:50:01 -0500

    Adding likeCount and dislikeCount virtuals

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,48 +1,64 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  likes: [
-    {
+const postSchema = new mongoose.Schema(
+  {
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
-  ],
-  comments: [
-    {
-      user: {
+    content: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    likes: [
+      {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
       },
-      content: {
-        type: String,
-        required: true,
+    ],
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
       },
-      createdAt: {
-        type: Date,
-        default: Date.now,
+    ],
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        content: {
+          type: String,
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
       },
-    },
-  ],
-},
+    ],
+  },
   {
     toJSON: {
       virtuals: true,
     },
     id: false,
-  });
+  }
+);
+
+postSchema.virtual("likeCount").get(function () {
+  return this.likes.length;
+});
+
+postSchema.virtual("dislikeCount").get(function () {
+  return this.dislikes.length;
+});
 
 const Post = mongoose.model("Post", postSchema);
 
