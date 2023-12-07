6ec066e7f65f8ccb9dce921f2fa9416cf0ebaabe
Robert Kaiser <paintballmanrob@gmail.com>
2023-12-06 21:13:30 -0500

    Add signUpFormReducer to rootReducer

--- client/src/features/rootReducer.js
+++ client/src/features/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import chatReducer from './chatSlice';
+import signUpFormReducer from './signUpFormSlice';
 
 const rootReducer = combineReducers({
   chat: chatReducer,
+  signUpForm: signUpFormReducer,
 });
 
 export default rootReducer;
