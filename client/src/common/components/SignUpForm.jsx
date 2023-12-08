980839caea4b53f4ab64b8bead33b356f16a159f
Robert Kaiser <paintballmanrob@gmail.com>
2023-12-07 21:47:08 -0500

    Refactor SignUpForm component

--- client/src/common/components/SignUpForm.jsx
+++ client/src/common/components/SignUpForm.jsx
@@ -16,43 +16,27 @@ function SignUpForm() {
   const {
     userFormData,
     isUploading,
-    showAlert,
-    uploadProgress,
-    checkUsernameEmailLoading,
     checkUsernameEmailError,
     userRegistrationError,
   } = useSelector((state) => state.signUpForm);
-
   const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadError, setUploadError] = useState(false);
 
+  // Handles changes in form inputs including file selection
   const handleInputChange = (e) => {
-    if (e.target.name === "profileImage") {
-      const file = e.target.files[0];
-      if (file) {
-        setSelectedFile(file); // Store the actual file object
-        dispatch(
-          updateFormData({
-            field: e.target.name,
-            value: { name: file.name, size: file.size, type: file.type },
-          })
-        );
-      } else {
-        setSelectedFile(null); // No file selected
-        dispatch(
-          updateFormData({
-            field: e.target.name,
-            value: defaultProfileImage, // Set to default image
-          })
-        );
-      }
-    } else {
-      dispatch(updateFormData({ field: e.target.name, value: e.target.value }));
-    }
+    const { name, value, files } = e.target;
+    const newValue =
+      name === "profileImage" && files[0]
+        ? { name: files[0].name, size: files[0].size, type: files[0].type }
+        : value;
+    dispatch(updateFormData({ field: name, value: newValue }));
+    if (name === "profileImage") setSelectedFile(files[0] || null);
   };
 
+  // Function to handle file upload
   const uploadFile = async () => {
+    // Define key using the selected file's name
     const key = `uploads/${selectedFile.name}`;
-    // Generate a unique key for the file
     try {
       const response = await fetch(
         `/presigned-url?key=${encodeURIComponent(key)}`
@@ -63,21 +47,14 @@ function SignUpForm() {
       return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
         xhr.open("PUT", presignedUrl, true);
-
-        xhr.upload.onprogress = (event) => {
-          if (event.lengthComputable) {
-            const progress = (event.loaded / event.total) * 100;
-            dispatch(setUploadProgress(progress));
-          }
-        };
-
+        xhr.upload.onprogress = (event) =>
+          dispatch(setUploadProgress((event.loaded / event.total) * 100));
         xhr.onload = () =>
           xhr.status === 200
             ? resolve(`https://branch-out-images.s3.amazonaws.com/${key}`)
             : reject(xhr.responseText);
         xhr.onerror = () => reject(xhr.statusText);
-
-        xhr.send(selectedFile); // Send the actual file
+        xhr.send(selectedFile);
       });
     } catch (error) {
       console.error("Error getting presigned URL:", error);
@@ -85,8 +62,7 @@ function SignUpForm() {
     }
   };
 
-  const [uploadError, setUploadError] = useState(false);
-
+  // Function to handle form submission
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     setUploadError(false);
@@ -99,35 +75,22 @@ function SignUpForm() {
           email: userFormData.email,
         })
       ).unwrap();
+      let imageUrl = selectedFile
+        ? await uploadFile()
+        : "https://branch-out-images.s3.us-east-2.amazonaws.com/uploads/user-ninja-solid.svg";
 
-      let imageUrl =
-        "https://branch-out-images.s3.us-east-2.amazonaws.com/uploads/user-ninja-solid.svg";
-      if (selectedFile) {
-        try {
-          const uploadedImageUrl = await uploadFile();
-          if (uploadedImageUrl) {
-            imageUrl = uploadedImageUrl;
-          }
-        } catch (uploadError) {
-          console.error("Error during file upload:", uploadError);
-          setUploadError(true);
-          return; // Exit if there is an upload error
-        }
-      }
-
-      // Register the user
+      // Register the user and reset the form
       await dispatch(
         uploadFileAndRegisterUser({ userFormData, imageUrl })
       ).unwrap();
-      dispatch(resetForm()); // Reset the form on successful registration
+      dispatch(resetForm());
     } catch (error) {
-      // Catch any errors from username/email check or registration
       console.error("Error during form submission:", error);
       setUploadError(true);
     }
   };
 
-  // JSX for the form
+  // JSX for rendering the form
   return (
     <>
       <form
@@ -139,7 +102,10 @@ function SignUpForm() {
           <img src={Logo} alt="Branch Out Logo" />
         </div>
 
+        {/* Form Fields */}
         <div>
+          {" "}
+          {/* Email Field */}
           <label htmlFor="email" className={styles.loginRequirement}>
             Email
           </label>
@@ -150,12 +116,13 @@ function SignUpForm() {
             name="email"
             id="email"
             onChange={handleInputChange}
-            value={userFormData.email} // Value obtained from Redux state
+            value={userFormData.email}
             required
           />
         </div>
-
         <div>
+          {" "}
+          {/* Username Field */}
           <label htmlFor="username" className={styles.loginRequirement}>
             Username
           </label>
@@ -166,12 +133,13 @@ function SignUpForm() {
             name="username"
             id="username"
             onChange={handleInputChange}
-            value={userFormData.username} // Value obtained from Redux state
+            value={userFormData.username}
             required
           />
         </div>
-
         <div>
+          {" "}
+          {/* Password Field */}
           <label htmlFor="password" className={styles.loginRequirement}>
             Password
           </label>
@@ -182,12 +150,13 @@ function SignUpForm() {
             name="password"
             id="password"
             onChange={handleInputChange}
-            value={userFormData.password} // Value obtained from Redux state
+            value={userFormData.password}
             required
           />
         </div>
-
         <div>
+          {" "}
+          {/* Bio Field */}
           <label htmlFor="bio" className={styles.loginRequirement}>
             Bio
           </label>
@@ -198,12 +167,13 @@ function SignUpForm() {
             cols="30"
             rows="4"
             onChange={handleInputChange}
-            value={userFormData.bio} // Value obtained from Redux state
+            value={userFormData.bio}
             placeholder="Enter Your Bio"
           ></textarea>
         </div>
-
         <div>
+          {" "}
+          {/* Profile Image Field */}
           <label htmlFor="profileImage" className={styles.loginRequirement}>
             Profile Picture
           </label>
@@ -212,24 +182,33 @@ function SignUpForm() {
             type="file"
             name="profileImage"
             id="profileImage"
-            onChange={handleInputChange} // Handle file input changes
+            onChange={handleInputChange}
           />
         </div>
 
+        {/* Upload Progress and Error Display */}
         {isUploading && (
-          <div>
-            <Spinner
-              color="#someColor" // Replace with actual color value
-              loading={isUploading}
-              size={150}
-              aria-label="Loading..."
-              data-testid="spinner"
-            />
-            <p>Uploading image... {uploadProgress}%</p>
+          <Spinner
+            color="#someColor"
+            loading={isUploading}
+            size={150}
+            aria-label="Loading..."
+            data-testid="spinner"
+          />
+        )}
+        {uploadError && (
+          <div className={styles.somethingWrong}>
+            Something went wrong with your sign up!
           </div>
         )}
+        {checkUsernameEmailError && (
+          <div className={styles.error}>{checkUsernameEmailError}</div>
+        )}
+        {userRegistrationError && (
+          <div className={styles.error}>{userRegistrationError}</div>
+        )}
 
-        {/* Submit button */}
+        {/* Submit Button */}
         <button
           type="submit"
           className={styles.signUpPageButton}
@@ -237,33 +216,6 @@ function SignUpForm() {
         >
           Submit
         </button>
-
-        {/* Error alert */}
-        {uploadError && (
-          <div className={styles.somethingWrong}>
-            Something went wrong with your sign up!
-          </div>
-        )}
-        {/* Display username/email existence error */}
-        {checkUsernameEmailError && (
-          <div className={styles.error}>{checkUsernameEmailError}</div>
-        )}
-
-        {/* Display user registration error */}
-        {userRegistrationError && (
-          <div className={styles.error}>{userRegistrationError}</div>
-        )}
-
-        {/* Display loading spinner */}
-        {checkUsernameEmailLoading && (
-          <Spinner
-            color="#00FF00" // Replace with actual color value
-            loading={checkUsernameEmailLoading}
-            size={150}
-            aria-label="Loading..."
-            data-testid="spinner"
-          />
-        )}
       </form>
     </>
   );
