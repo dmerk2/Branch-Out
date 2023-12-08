14528717a6875d4aa9259925a50c69eaa9dbcdd6
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-12-07 20:27:44 -0500

    Created OtherUserProfile and made it dynamic

--- client/src/common/components/ProfileUserDisplay.jsx
+++ client/src/common/components/ProfileUserDisplay.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import UserDiamond from "../components/UserDiamond.jsx";
 import styles from "../../styles/ProfileUserDisplay.module.css";
 import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom';
 import { GET_USER_INFO } from '../utils/queries'; // Adjust the import path
 
 export default function ProfileUserDisplay() {
-
-  const { loading, error, data } = useQuery(GET_USER_INFO);
+  const { id } = useParams(); // Get the user ID from the URL
+  
+  const { loading, error, data } = useQuery(GET_USER_INFO, {
+    variables: { id }, // Pass the user ID to the query
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
     return (
             <div className={styles.userDiamond_Horizontal_Container}>
-              <UserDiamond />
+              <UserDiamond profileImage={data.user.profileImage} />
               <p className={styles.profileUsername}>{data.user.username}</p>
             </div>
 
--- client/src/common/components/UserDiamond.jsx
+++ client/src/common/components/UserDiamond.jsx
@@ -2,10 +2,16 @@ import styles from "../../styles/UserDiamond.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 
-export default function UserDiamond() {
+export default function UserDiamond({ profileImage }) {
+    profileImage = profileImage || null;
+    console.log("Here is the Profile Picture:" + profileImage);
     return (
-        <div className= { styles.diamondShape}>
-                <FontAwesomeIcon icon={faUserSecret} className= { styles.userIcon } />
+        <div className={styles.diamondShape}>
+            {profileImage ? (
+                <img src={profileImage} alt="User" className={styles.userIcon} />
+            ) : (
+                <FontAwesomeIcon icon={faUserSecret} className={styles.userIcon} />
+            )}
         </div>
-     );
+    );
 }
