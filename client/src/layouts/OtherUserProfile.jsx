7a3c91188ab952c95407572373d4b822d2380bff
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-12-10 17:45:34 -0500

    Added Icons to buttons on other user profiles

--- client/src/layouts/OtherUserProfile.jsx
+++ client/src/layouts/OtherUserProfile.jsx
@@ -1,14 +1,20 @@
-import React from "react";
 import styles from "../styles/Profile.module.css";
+import { faCodeCompare, faLink, faCode } from '@fortawesome/free-solid-svg-icons';
 import ProfileUserDisplay from "../common/components/ProfileUserDisplay.jsx";
 import ViewFriendsList from "../common/components/ViewFriendsList.jsx";
-import AddFriendSquare from "../common/components/AddFriendSquare.jsx";
 import AboutMe from "../common/components/AboutMe.jsx";
 import DiamondButton from "../common/components/DiamondButton.jsx";
 import SendMessageButton from "../common/components/SendMessage"
 import UserPosts from "../common/components/UserPosts.jsx";
 
 export default function OtherUserProfile() {
+  const buttons = [
+    { icon: faCodeCompare, url: 'https://github.com/' },
+    { icon: faLink, url: 'https://www.linkedin.com/' },
+    { icon: faCode, url: 'https://codepen.io/' },
+    // Add more objects as needed
+  ];
+
   return (
 
       <div className= {styles.profilePageRow}>
@@ -19,18 +25,16 @@ export default function OtherUserProfile() {
           <div className={styles.profileFriendsTracker}>
             < ViewFriendsList />
           </div>
-          <div className={styles.profileAddFriends}>
-            <AddFriendSquare />
-            <AddFriendSquare />
-          </div>
         </div>
         <div className= { styles.profilePageColumn}>
           <AboutMe />
           <div className={styles.profileButtonRow}>
-            <div className={styles.profileButton}><DiamondButton /></div>
-            <div className={styles.profileButton}><DiamondButton /></div>
-            <div className={styles.profileButton}><DiamondButton /></div>
-          </div>
+          {buttons.map((button, index) => (
+            <div className={styles.profileButton} key={index}>
+              <DiamondButton icon={button.icon} url={button.url} />
+            </div>
+          ))}
+        </div>
           <div><UserPosts/></div>
         </div>
         <div className= { styles.profilePageColumn}>
