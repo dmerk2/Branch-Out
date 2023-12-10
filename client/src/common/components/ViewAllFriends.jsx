7721cadc3466702de4976eb212ba81e03eec8ecc
dmerk2 <dan.merkin@gmail.com>
2023-12-10 15:33:26 -0500

    Added styling to dropdown

--- client/src/common/components/ViewAllFriends.jsx
+++ client/src/common/components/ViewAllFriends.jsx
@@ -1,40 +1,40 @@
-import AddFriendSquare from './AddFriendSquare';
+import AddFriendSquare from "./AddFriendSquare";
 import { useParams } from "react-router-dom";
 import auth from "../utils/auth";
 import { GET_USER_INFO } from "../utils/queries";
 import { useQuery } from "@apollo/client";
 
 const AllFriendsList = () => {
-    // Get the user ID from the URL
-    const { id } = useParams();
-    let userData = {};
-  
-    if (!id) {
-      const user = auth.getProfile().data._id;
-      const { loading, error, data } = useQuery(GET_USER_INFO, {
-        variables: { id: user },
-      });
-      userData = data;
-  
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error: {error.message}</p>;
-  
-      console.log(userData.user.friends);
-    } else {
-      const { loading, error, data } = useQuery(GET_USER_INFO, {
-        // Pass the user ID to the query
-        variables: { id },
-      });
-      userData = data;
-  
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error: {error.message}</p>;
-  
-      console.log(userData.user.friends);
-    }
-  
-    const friends = userData?.user.friends || [];
-  
+  // Get the user ID from the URL
+  const { id } = useParams();
+  let userData = {};
+
+  if (!id) {
+    const user = auth.getProfile().data._id;
+    const { loading, error, data } = useQuery(GET_USER_INFO, {
+      variables: { id: user },
+    });
+    userData = data;
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    console.log(userData.user.friends);
+  } else {
+    const { loading, error, data } = useQuery(GET_USER_INFO, {
+      // Pass the user ID to the query
+      variables: { id },
+    });
+    userData = data;
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    console.log(userData.user.friends);
+  }
+
+  const friends = userData?.user.friends || [];
+
   return (
     <div>
       <h2>All Friends</h2>
