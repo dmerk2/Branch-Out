1d2e7e9f5e0be4f788a9890ae0b6d59201d51769
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-12-11 22:45:43 -0500

    Corrected URL for friends on Home Page

--- client/src/common/components/HomepageUserDiamond.jsx
+++ client/src/common/components/HomepageUserDiamond.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import styles from "../../styles/HomepageUserDiamond.module.css";
+import { useQuery } from '@apollo/client';
+import { useParams} from 'react-router-dom';
+import { GET_USER_INFO } from '../utils/queries';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserSecret, faUserGroup, faMessage, faEnvelope, faRightToBracket} from "@fortawesome/free-solid-svg-icons";
 import HomePageDiamondButton from "./HomePageDiamondButton";
 import AuthService from "../utils/auth"
 import BranchOutLogo from "../../assets/images/BranchOut_Logo.svg";
+import auth from '../utils/auth';
 
 export default function HomepageUserDiamond() {
+  const { id } = useParams();
+  const isOwnProfile = !id; // If there is no ID parameter, it's the user's own profile
+
+  // Get the user ID based on whether it's their own profile or another user's profile
+  const userId = isOwnProfile ? auth.getProfile().data._id : id;
+
+  const { loading, error, data } = useQuery(GET_USER_INFO, {
+    variables: { id: userId },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const userData = data;
+
   const isLoggedIn = AuthService.loggedIn();
 
   const buttons = isLoggedIn
     ? [
-        { icon: faUserGroup, url: '/friends' },
+        { icon: faUserGroup, url: `/${userId}/friends` },
         { icon: faMessage, url: '/chatroom' },
         { icon: faEnvelope, url: '/profile' },
         // Add more objects as needed
