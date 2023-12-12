9adf783186f08dcab666193492a652c32b166407
dmerk2 <dan.merkin@gmail.com>
2023-12-11 22:13:25 -0500

    Updating user form bugs

--- client/src/common/components/EditProfileForm.jsx
+++ client/src/common/components/EditProfileForm.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { GET_USER_INFO } from "../utils/queries";
-import auth from '../utils/auth'
+import auth from "../utils/auth";
 import { useQuery } from "@apollo/client";
 import { UPDATE_USER } from "../utils/mutations";
 import { useMutation } from "@apollo/client";
@@ -9,28 +9,32 @@ const EditProfileForm = () => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
-    password: "",
     bio: "",
   });
-  const [updateUser] = useMutation(UPDATE_USER)
+  const [updateUser] = useMutation(UPDATE_USER);
   const user = auth.getProfile().data._id;
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
+  console.log("User:", user)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const result = await updateUser({
-        variables: { id: user }
-      })
-      console.log('Profile updated successfully', result);
+        variables: {
+          _id: user,
+          username: formData.name,
+          email: formData.email,
+          bio: formData.bio,
+        },
+      });
+      console.log("Profile updated successfully", result);
     } catch (error) {
-      console.error('Error updating profile:', error);
+      console.error("Error updating profile:", error);
     }
   };
-
 
   const { loading, error, data } = useQuery(GET_USER_INFO, {
     variables: { id: user },
@@ -41,7 +45,6 @@ const EditProfileForm = () => {
       setFormData({
         name: data.user.username || "",
         email: data.user.email || "",
-        password: data.user.password || "",
         bio: data.user.bio || "",
       });
     }
@@ -50,7 +53,7 @@ const EditProfileForm = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  console.log(data)
+  console.log(data);
 
   return (
     <form onSubmit={handleSubmit}>
@@ -63,23 +66,13 @@ const EditProfileForm = () => {
           onChange={handleChange}
         />
       </label>
-      
+
       <label>
         Email:
         <input
           type="text"
           name="email"
           value={formData.email}
-          onChange={handleChange}
-        />
-      </label>
-
-      <label>
-        Password:
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
           onChange={handleChange}
         />
       </label>
