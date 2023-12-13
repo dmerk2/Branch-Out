04cd6085bf3739348108021360df6e86a90b7c47
Robert Kaiser <paintballmanrob@gmail.com>
2023-12-13 16:04:26 -0500

    Refactor Profile component and add user info

--- client/src/layouts/Profile.jsx
+++ client/src/layouts/Profile.jsx
@@ -1,5 +1,9 @@
 import styles from "../styles/Profile.module.css";
-import { faCodeCompare, faLink, faCode } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCodeCompare,
+  faLink,
+  faCode,
+} from "@fortawesome/free-solid-svg-icons";
 import ProfileUserDisplay from "../common/components/ProfileUserDisplay.jsx";
 import ViewFriendsList from "../common/components/ViewFriendsList.jsx";
 import AboutMe from "../common/components/AboutMe.jsx";
@@ -7,20 +11,36 @@ import DiamondButton from "../common/components/DiamondButton.jsx";
 import WhatsOnYourMind from "../common/components/WhatsOnYourMind.jsx";
 import UnreadMessageBoard from "../common/components/UnreadMessageBoard.jsx";
 import UserPosts from "../common/components/UserPosts.jsx";
+import { useQuery } from "@apollo/client";
+import { GET_USER_INFO } from "../common/utils/queries";
+import auth from "../common/utils/auth";
 
 export default function Profile() {
   const buttons = [
-    { icon: faCodeCompare, url: 'https://github.com/' },
-    { icon: faLink, url: 'https://www.linkedin.com/' },
-    { icon: faCode, url: 'https://codepen.io/' },
+    { icon: faCodeCompare, url: "https://github.com/" },
+    { icon: faLink, url: "https://www.linkedin.com/" },
+    { icon: faCode, url: "https://codepen.io/" },
     // Add more objects as needed
   ];
+
+  let id = auth.getProfile().data._id;
+
+  const { loading, error, data } = useQuery(GET_USER_INFO, {
+    variables: { id: id },
+  });
+
+  // Declare userInfo here
+  const userInfo = data && data.user;
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+  if (!userInfo) return <p>Loading...</p>; // Now userInfo is already declared
 
   return (
     <div className={styles.profilePageRow}>
       <div className={styles.profilePageColumn}>
         <div className={styles.profileUserSection}>
-          <ProfileUserDisplay />
+          <ProfileUserDisplay userInfo={userInfo} />
         </div>
         <div className={styles.profileFriendsTracker}>
           <ViewFriendsList />
