17289122b7dbf21124631172aff30fc48693db2b
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-12-12 21:03:02 -0500

    Fixed bug with Other User Profile. Like/Dislike counter is broken

--- client/src/common/components/UserPosts.jsx
+++ client/src/common/components/UserPosts.jsx
@@ -1,174 +1,126 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_USER_INFO } from "../utils/queries";
-import {
-  LIKE_POST,
-  DISLIKE_POST,
-  ADD_COMMENT,
-  UNDISLIKE_POST,
-  UNLIKE_POST,
-} from "../utils/mutations";
+import { LIKE_POST, DISLIKE_POST, ADD_COMMENT } from "../utils/mutations";
 import auth from "../utils/auth";
 import styles from "../../styles/RecentPost.module.css";
 import { useParams } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbsDown, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import AuthService from "../utils/auth";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+
 
 const UserPosts = () => {
   const { id } = useParams();
-  console.log("ID from useParams:", id);
-  const userProfile = auth.getProfile();
-  const loggedinuser = userProfile ? userProfile.data._id : null;
+  let userData = {};
 
   const [likePostMutation] = useMutation(LIKE_POST);
   const [dislikePostMutation] = useMutation(DISLIKE_POST);
-  const [unlikePostMutation] = useMutation(UNLIKE_POST);
-  const [undislikePostMutation] = useMutation(UNDISLIKE_POST);
   const [addCommentMutation] = useMutation(ADD_COMMENT, {
-    refetchQueries: [ ADD_COMMENT, GET_USER_INFO ],
+    refetchQueries: [ADD_COMMENT, GET_USER_INFO],
   });
+
+  const [likeCount, setLikeCount] = useState(0);
+  const [dislikeCount, setDislikeCount] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [addingComment, setAddingComment] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
   const [isDisliked, setIsDisliked] = useState(false);
-  const [addingComment, setAddingComment] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [commentContent, setCommentContent] = useState("");
-
-  const { loading, error, data } = useQuery(GET_USER_INFO, {
-    variables: { id: id || loggedinuser },
-  });
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
-  const userPosts = data?.user.posts || [];
-
+  const [userAction, setUserAction] = useState(null);
+  
   const handleLikePost = async (postId) => {
-    const post = userPosts.find((p) => p._id === postId);
-    if (!post || !post.likes) {
-      return;
-    }
-    const userHasLiked = post.likes.some((like) => like._id === loggedinuser);
-
-    if (userHasLiked) {
-      await unlikePostMutation({
-        variables: { postId, userId: loggedinuser },
-        refetchQueries: [
-          { query: GET_USER_INFO, variables: { id: id || loggedinuser } },
-        ],
-      });
+    if (userAction === "like") {
+      setUserAction(null);
+      setIsLiked(false);
+      setLikeCount(likeCount - 1);
     } else {
-      await likePostMutation({
-        variables: { postId, userId: loggedinuser },
-        refetchQueries: [
-          { query: GET_USER_INFO, variables: { id: id || loggedinuser } },
-        ],
-      });
-    }
-  };
-  const handleDislikePost = async (postId) => {
-    const post = userPosts.find((p) => p._id === postId);
-    if (!post || !post.dislikes) {
-      return;
-    }
-    const userHasDisliked = post.dislikes.some(
-      (dislike) => dislike._id === loggedinuser
-    );
-
-    if (userHasDisliked) {
-      await undislikePostMutation({
-        variables: { postId, userId: loggedinuser },
-        refetchQueries: [
-          { query: GET_USER_INFO, variables: { id: id || loggedinuser } },
-        ],
-      });
-    } else {
-      await dislikePostMutation({
-        variables: { postId, userId: loggedinuser },
-        refetchQueries: [
-          { query: GET_USER_INFO, variables: { id: id || loggedinuser } },
-        ],
-      });
-    }
-  };
-
-  const handleAddComment = async (postId, content) => {
-    // Update the function parameters
-    try {
-      // Check if content is not empty
-      if (!content.trim()) {
-        console.error("Comment content cannot be empty");
-        return;
+      if (userAction === "dislike") {
+        setDislikeCount(dislikeCount - 1);
+        setIsDisliked(false);
       }
-
-      setAddingComment(true);
-
-      const { data } = await addCommentMutation({
-        variables: {
-          post: postId,
-          user: auth.getProfile().data._id,
-          content: content,
-        },
+      setUserAction("like");
+      setIsLiked(true);
+      setLikeCount(likeCount + 1);
+    }
+  
+    try {
+      const { data, errors } = await likePostMutation({
+        variables: { postId },
       });
-
-      if (data.addComment) {
-        console.log("Comment added successfully:", data.addComment);
-        setAddingComment(false);
-        setCommentContent("");
+  
+      if (errors) {
+        console.error("Error liking post:", errors);
+      } else if (data.likePost && data.likePost.message) {
+        alert(data.likePost.message);
+      } else {
+        setLikeCount(data.likePost.likeCount);
       }
     } catch (error) {
-      console.error("Error adding comment:", error);
-      setAddingComment(false);
+      console.error("Error liking post:", error);
     }
   };
 
-  const CommentModal = ({ onClose, onSubmit }) => {
-    const [localCommentContent, setLocalCommentContent] = useState("");
+const handleDislikePost = async (postId) => {
+  if (userAction === "dislike") {
+    setUserAction(null);
+    setIsDisliked(false);
+    setDislikeCount(dislikeCount - 1);
+  } else {
+    if (userAction === "like") {
+      setLikeCount(likeCount - 1);
+      setIsLiked(false);
+    }
+    setUserAction("dislike");
+    setIsDisliked(true);
+    setDislikeCount(dislikeCount + 1);
+  }
 
-    const handleCommentSubmit = () => {
-      console.log("Comment Content Length:", localCommentContent.length);
+  try {
+    const { data, errors } = await dislikePostMutation({
+      variables: { postId },
+    });
+  
+    if (errors) {
+      console.error("Error disliking post:", errors);
+    } else if (data.dislikePost && data.dislikePost.message) {
+      alert(data.dislikePost.message);
+    } else {
+      setDislikeCount(data.dislikePost.dislikeCount);
+    }
+  } catch (error) {
+    console.error("Error disliking post:", error);
+  }
+  
+};
 
-      // Check if localCommentContent is not empty
-      if (!localCommentContent.trim()) {
-        console.error("Comment content cannot be empty");
-        return;
-      }
+let data;
+const isLoggedIn = AuthService.loggedIn();
 
-      // Call onSubmit with postId and content
-      onSubmit(localCommentContent);
-      onClose();
-    };
+if (!id) {
+  const user = auth.getProfile().data._id;
+  const result = useQuery(GET_USER_INFO, {
+    variables: { id: user },
+  });
+  data = result.data;
 
-    return (
-      <div className={styles.modal}>
-        <div className={styles.modalHeader}>
-          <p className={styles.modalText}>Post Your Comment!</p>
-          <button className={styles.closeButton} onClick={() => onClose()}>
-            &times;
-          </button>
-        </div>
-        <textarea
-          className={styles.modalTextArea}
-          rows="4"
-          cols="50"
-          value={localCommentContent}
-          onChange={(e) => setLocalCommentContent(e.target.value)}
-        />
-        <br />
-        <div className={styles.modalButtons}>
-          <button
-            className={styles.commentButton}
-            onClick={handleCommentSubmit}
-          >
-            Submit Comment
-          </button>
-        </div>
-      </div>
-    );
-  };
+  if (result.loading) return <p>Loading...</p>;
+  if (result.error) return <p>Error: {result.error.message}</p>;
+} else {
+  const result = useQuery(GET_USER_INFO, {
+    variables: { id },
+  });
+  data = result.data;
+
+  if (result.loading) return <p>Loading...</p>;
+  if (result.error) return <p>Error: {result.error.message}</p>;
+}
+
+const userPosts = data?.user.posts || [];
 
   return (
     <div>
       <h2 className={styles.postHeader}>User Posts</h2>
+      
       {userPosts.map((post) => (
         <div className={styles.postContainer} key={post._id}>
           <div className={styles.userDetails}>
@@ -182,20 +134,21 @@ const UserPosts = () => {
           <div className={styles.userPost}>
             <p className={styles.postContent}>{post.content}</p>
           </div>
-          
-          <button
-            className={styles.commentButton}
-            onClick={() => setShowModal(true)}
-          >
-            Add a Comment
-          </button>
-          
-          <button
-            className={styles.commentButtonGone}
-            onClick={() => setShowModal(true)}
-          >
-            Add a Comment
-          </button>
+          {isLoggedIn ? (
+            <button
+              className={styles.commentButton}
+              onClick={() => setShowModal(true)}
+            >
+              Add a Comment
+            </button>
+          ) : (
+            <button
+              className={styles.commentButtonGone}
+              onClick={() => setShowModal(true)}
+            >
+              Add a Comment
+            </button>
+          )}
           {/* Modal */}
           {showModal && (
             <CommentModal
