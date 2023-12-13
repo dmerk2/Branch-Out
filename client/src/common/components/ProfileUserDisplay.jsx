b95a23d8375d33aa1e73c08c9bfd2a113a35390e
Robert Kaiser <paintballmanrob@gmail.com>
2023-12-13 14:53:31 -0500

    Refactor ProfileUserDisplay component

--- client/src/common/components/ProfileUserDisplay.jsx
+++ client/src/common/components/ProfileUserDisplay.jsx
@@ -1,40 +1,12 @@
-import React from 'react';
+import React from "react";
 import UserDiamond from "../components/UserDiamond.jsx";
 import styles from "../../styles/ProfileUserDisplay.module.css";
-import { useQuery } from '@apollo/client';
-import { useParams } from 'react-router-dom';
-import { GET_USER_INFO } from '../utils/queries'; // Adjust the import path
-import auth from '../utils/auth.js';
 
-export default function ProfileUserDisplay() {
-
-  const { id } = useParams(); // Get the user ID from the URL
-  let userData = {};
-
-  if (!id) {
-    const user = auth.getProfile().data._id;
-    const { loading, error, data } = useQuery(GET_USER_INFO, {variables: {"id": user}});
-    userData = data;
-  
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
-
-  } else {
-      const { loading, error, data } = useQuery(GET_USER_INFO, {
-        variables: { id }, // Pass the user ID to the query
-      });
-      userData = data;
-    
-    
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error: {error.message}</p>;
-  }
-  
-    return (
-            <div className={styles.userDiamond_Horizontal_Container}>
-              <UserDiamond profileImage={userData.user.profileImage} />
-              <p className={styles.profileUsername}>{userData.user.username}</p>
-            </div>
-
-     );
+export default function ProfileUserDisplay({ userInfo }) {
+  return (
+    <div className={styles.userDiamond_Horizontal_Container}>
+      <UserDiamond profileImage={userInfo.profileImage} />
+      <p className={styles.profileUsername}>{userInfo.username}</p>
+    </div>
+  );
 }
