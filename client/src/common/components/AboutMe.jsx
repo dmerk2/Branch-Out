5d1b785be31fe3798aa6c5c877fd9db3e78fabee
Robert Kaiser <paintballmanrob@gmail.com>
2023-12-13 14:53:24 -0500

    Refactor AboutMe component to improve code readability and efficiency

--- client/src/common/components/AboutMe.jsx
+++ client/src/common/components/AboutMe.jsx
@@ -4,65 +4,38 @@ import { useParams } from "react-router-dom";
 import { GET_USER_INFO } from "../utils/queries";
 import auth from "../utils/auth";
 import { useNavigate } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
-
-
-
+import { useLocation } from "react-router-dom";
 
 export default function AboutMe() {
-  // Get the user ID from the URL
   const { id } = useParams();
-  let userData = {};
+  const isLoggedIn = auth.loggedIn();
+  const userId = isLoggedIn ? auth.getProfile().data._id : id;
   const navigate = useNavigate();
-  const currentUser = auth.getProfile().data._id;
   const location = useLocation();
 
-  if (!id) {
-    const user = auth.getProfile().data._id;
-    const { loading, error, data } = useQuery(GET_USER_INFO, {
-      variables: { id: user },
-    });
-    userData = data;
+  const { loading, error, data } = useQuery(GET_USER_INFO, {
+    variables: { id: userId },
+  });
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
-    return (
-      <div className={styles.bigGreenSquare}>
-        <p className={styles.aboutMe}>About Me</p>
-        <p className={styles.userDescription}>{userData.user.bio}</p>
-        {location.pathname === '/profile' && (
-          <button
-            onClick={() => navigate(`${userData.user._id}/edit`)}
-            className={styles.editButton}
-          >
-            Edit
-          </button>
-        )}
-      </div>
-    );
-  } else {
-    const { loading, error, data } = useQuery(GET_USER_INFO, {
-      variables: { id },
-    });
-    userData = data;
+  const userData = data;
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
-
-    return (
-      <div className={styles.bigGreenSquare}>
-        <p className={styles.aboutMe}>About Me</p>
-        <p className={styles.userDescription}>{userData.user.bio}</p>
-        {currentUser === id && location.pathname === '/profile' && (
-          <button
-            onClick={() => navigate(`${userData.user._id}/edit`)}
-            className={styles.editButton}
-          >
-            Edit
-          </button>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className={styles.bigGreenSquare}>
+      <p className={styles.aboutMe}>About Me</p>
+      <p className={styles.userDescription}>
+        {userData && userData.user ? userData.user.bio : "Loading..."}
+      </p>
+      {isLoggedIn && location.pathname === "/profile" && (
+        <button
+          onClick={() => navigate(`${userData.user._id}/edit`)}
+          className={styles.editButton}
+        >
+          Edit
+        </button>
+      )}
+    </div>
+  );
 }
