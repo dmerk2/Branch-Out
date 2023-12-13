fc18ab6e8211bf33e603fc73f8be4b5223ea35bb
Robert Kaiser <paintballmanrob@gmail.com>
2023-12-13 16:05:20 -0500

    Add console logs and loading message to ProfileUserDisplay component

--- client/src/common/components/ProfileUserDisplay.jsx
+++ client/src/common/components/ProfileUserDisplay.jsx
@@ -3,6 +3,9 @@ import UserDiamond from "../components/UserDiamond.jsx";
 import styles from "../../styles/ProfileUserDisplay.module.css";
 
 export default function ProfileUserDisplay({ userInfo }) {
+  console.log("ProfileUserDisplay:", userInfo);
+  console.log(userInfo);
+  if (!userInfo) return <p>Loading...</p>;
   return (
     <div className={styles.userDiamond_Horizontal_Container}>
       <UserDiamond profileImage={userInfo.profileImage} />
--- client/src/layouts/OtherUserProfile.jsx
+++ client/src/layouts/OtherUserProfile.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styles from "../styles/Profile.module.css";
 import {
@@ -16,25 +17,48 @@ import { useQuery } from "@apollo/client";
 import { GET_USER_INFO } from "../common/utils/queries";
 
 export default function OtherUserProfile() {
-  const { id } = useParams();
+  let { id } = useParams();
+
+  // If there's no id in the URL parameters, get the id of the currently logged in user
+  if (!id && auth.loggedIn()) {
+    id = auth.getProfile().data._id;
+  }
+  console.log(id);
   const buttons = [
     { icon: faCodeCompare, url: "https://github.com/" },
     { icon: faLink, url: "https://www.linkedin.com/" },
     { icon: faCode, url: "https://codepen.io/" },
   ];
+
   const { loading, error, data } = useQuery(GET_USER_INFO, {
     variables: { id: id },
   });
 
+  // Add a state to hold the user's info
+  const [userInfo, setUserInfo] = useState(null);
+
+  // Update the userInfo state when data changes
+  useEffect(() => {
+    if (data && data.user) {
+      setUserInfo(data.user);
+    }
+  }, [data]);
+
+  console.log("After useQuery");
+  console.log("loading:", loading);
+  console.log("error:", error);
+  console.log("data:", data);
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (error) return <p>Error :</p>;
+
+  if (!userInfo) return <p>Loading...</p>; // Use userInfo here
 
   return (
     <div>
       <div className={styles.profilePageRow}>
         <div className={styles.profilePageColumn}>
           <div className={styles.profileUserSection}>
-            <ProfileUserDisplay userInfo={data.user} />
+            <ProfileUserDisplay userInfo={userInfo} /> {/* Use userInfo here */}
           </div>
           <div>
             <AddFriendButtonWithQuery userId={id} />
@@ -44,7 +68,7 @@ export default function OtherUserProfile() {
           </div>
         </div>
         <div className={styles.profilePageColumn}>
-          <AboutMe userInfo={data.user} />
+          <AboutMe userInfo={userInfo} /> {/* Use userInfo here */}
           <div className={styles.profileButtonRow}>
             {buttons.map((button, index) => (
               <div className={styles.profileButton} key={index}>
